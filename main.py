0314efd474b9907655c97c6a0f3705a136282005
Yoskex <merezhnikovmaksimm@yandex.ru>
2025-01-25 14:58:58 +0300

    hp bar for enemy + bullet hits on enemy

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ class Player(pygame.sprite.Sprite):
             self.velocity_x = -self.speed
 
         if self.velocity_x != 0 and self.velocity_y != 0:
-            self.velocity_x /= 2 ** 0.5
-            self.velocity_y /= 2 ** 0.5
+            self.velocity_x /= 2
+            self.velocity_y /= 2
 
     def move(self):
         self.rect = self.rect.move(self.velocity_x, self.velocity_y)
@@ -58,6 +58,18 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 1280 - self.rect.width
         self.rect.y = 720 - self.rect.height
+        self.hp = 100
+
+    def update(self):
+        pass
+
+    def draw_hp_bar(self, surface):
+        hp_bar_length = 50
+        hp_ratio = self.hp / 100
+        hp_rect = pygame.Rect(self.rect.x, self.rect.y - 10, hp_bar_length * hp_ratio, 5)
+
+        pygame.draw.rect(surface, (255, 0, 0), (self.rect.x, self.rect.y - 10, hp_bar_length, 5))
+        pygame.draw.rect(surface, (0, 255, 0), hp_rect)
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -79,7 +91,8 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
 
-        if self.rect.bottom < 0 or self.rect.top > 720 or self.rect.right < 0 or self.rect.left > 1280:
+        if (self.rect.bottom < 0 or self.rect.top > 720 or
+                self.rect.right < 0 or self.rect.left > 1280):
             self.kill()
 
 
@@ -91,8 +104,10 @@ if __name__ == '__main__':
 
     all_sprites = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+
     player = Player(all_sprites)
-    enemy = Enemy(all_sprites)
+    enemy = Enemy(all_sprites, enemies)
 
     clock = pygame.time.Clock()
 
@@ -104,10 +119,24 @@ if __name__ == '__main__':
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouse_pos = pygame.mouse.get_pos()
-                    bullet = Bullet(player.rect.center, mouse_pos, all_sprites)
+                    bullet = Bullet(player.rect.center, mouse_pos, all_sprites, bullets)
 
         all_sprites.update()
+
+        for bullet in bullets:
+            hit_enemies = pygame.sprite.spritecollide(bullet, enemies, False)
+
+
+            for enemy in hit_enemies:
+                enemy.hp -= 50
+                bullet.kill()
+                if enemy.hp <= 0:
+                    enemy.kill()
+
         all_sprites.draw(screen)
+
+        for enemy in enemies:
+            enemy.draw_hp_bar(screen)
 
         pygame.display.update()
         clock.tick(60)
